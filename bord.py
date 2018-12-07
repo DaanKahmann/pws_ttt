d82fa60fb514ce24f2ae73d469088561e63d141a
sjourdbeans <sjoerdbuitjes@gmail.com>
2018-12-07 23:14:18 +0100

    wie begint en tegen wie je wil spelen zitten niet meer in spel. maar de gelijk functie werkt niet!!!

--- bord.py
+++ bord.py
@@ -1,19 +1,15 @@
 import speler
 import spelerRandom
 
+
 class regels:
-    def spel(self):
+
+    def spel(self, spelTegen, x):
         bord = list(range(1, 10))  # Maak het bord
-        x = regels.wie_begint(self)
-        validInput = False
-        spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
-        while not validInput:
-            if spelTegen in ("1", "2", "3", "4"):
-                validInput = True
-            else:
-                spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
+        #x = regels.wie_begint(self)
 
-
+        #spelTegen = regels.tegen_wie(self)          #haalt variabele spelTegen uit de functie tegen_wie,
+                                                    #moet voor zorgen dat functie niet wordt aangeroepen in spel
         print("\n", bord[:3], "\n", bord[3:6], "\n", bord[6:])  # laat bord zien
 
         for i in range(9):
@@ -34,13 +30,24 @@ class regels:
             print("\n", bord[:3], "\n", bord[3:6], "\n", bord[6:])  # laat bord met wijziging zien
 
             if regels.winnaar(self, beurtX, bord):
-                regels.spel(self)
-            elif x == 9 :
+
+                break
+            elif x ==9:
                 print("It's a Tie")
                 break
             x = x + 1
 
 
+    def tegen_wie(self):
+        validInput = False
+        spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
+        while not validInput:
+            if spelTegen in ("1", "2", "3", "4"):
+                validInput = True
+            else:
+                spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
+
+        return spelTegen
 
     def wie_begint(self):
         input_valid = False
