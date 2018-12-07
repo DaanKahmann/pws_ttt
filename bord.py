da05743969f9301b0900b2f53467f3c112f3d916
Daan <daankahmann@live.nl>
2018-12-07 11:14:55 +0100

    iets overzichtelijker gemaakt van de opties. De opties zijn nu 1,2,3,4.

--- bord.py
+++ bord.py
@@ -6,24 +6,24 @@ class regels:
         bord = list(range(1, 10))  # Maak het bord
         x = regels.wie_begint(self)
         validInput = False
-        spelTegen = input("Tegen wie wil je spelen? kies uit: O random, X random, allebei random of allebei speler ")
+        spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
         while not validInput:
-            if spelTegen in ("allebei speler", "allebei random", "O random", "X random"):
+            if spelTegen in ("1", "2", "3", "4"):
                 validInput = True
             else:
-                spelTegen = input("Tegen wie wil je spelen? kies uit: O random, X random, allebei random of allebei speler ")
+                spelTegen = input("Tegen wie wil je spelen? kies uit: 1) O random 2) X random 3) allebei random 4) allebei speler ")
 
         print("\n", bord[:3], "\n", bord[3:6], "\n", bord[6:])  # laat bord zien
 
         for i in range(9):
             if x % 2 == 0:  # even getallen: 'X'
-                if spelTegen in ("X random", "allebei random"):
+                if spelTegen in ("2", "3"):
                     spelerRandom.spelerrandom.spelerRobot2(self, bord)
                 else:
                     speler.playermens.spelerX(self, bord)
                 beurtX = True
             else:  # oneven getallen 'O'
-                if spelTegen in ("O random", "allebei random"):
+                if spelTegen in ("1", "3"):
                     spelerRandom.spelerrandom.spelerRobot(self, bord)
                 else:
                     speler.playermens.spelerO(self, bord)
