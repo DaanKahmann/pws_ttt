44b5985b43bb252273249cd9b32b4e4b5a8f8a1e
Daan <daankahmann@live.nl>
2018-12-17 18:13:08 +0100

    tic_tac_toe is nu een functie niet een class, ik denk dat dit handiger is dan een class voor alpha_build

--- alpha_build.py
+++ alpha_build.py
@@ -2,16 +2,14 @@
 import bord
 
 
-class tic_tac_toe:
-    def __init__(self):
-        # bord_grootte = int(input("Hoe groot moet het bord zijn? "))
-        print("Welkom bij ons geweldige spel, veel plezier!")
-        spelTegen = bord.regels.tegen_wie(self)
+def tic_tac_toe(self=None):
+    # bord_grootte = int(input("Hoe groot moet het bord zijn? "))
+    print("Welkom bij ons geweldige spel, veel plezier!")
+    spelTegen = bord.regels.tegen_wie(self)
 
-        x = bord.regels.wie_begint(self)
+    x = bord.regels.wie_begint(self)
 
-        bord.regels(spelTegen, x)
-
+    bord.regels(spelTegen, x)
 
 
 tic_tac_toe()
