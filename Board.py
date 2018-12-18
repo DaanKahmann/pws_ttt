0e3418a127857d53d41465c64164822ad4a0c087
Daan <daankahmann@live.nl>
2018-12-18 21:17:51 +0100

    Board.py toegevoegd

---
+++ Board.py
@@ -0,0 +1,15 @@
+
+
+class Board:
+    def __init__(self):
+
+
+    def winner(self):   #geeft aan wie wint of geeft aan dat het een gelijkspel is.
+
+
+    def mogelijkheden(self):    #geeft de mogelijkheden die de speler heeft om een X of een O neer te zetten
+
+
+    def draw_board(self):   #geeft het bord (we weten niet zeker of deze wel nodig is)
+
+
