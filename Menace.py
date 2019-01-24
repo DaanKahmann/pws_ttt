454b69da658f7af013113cdd9f4e49e339de85dc
sjourdbeans <sjoerdbuitjes@gmail.com>
2019-01-24 21:25:29 +0100

    een poging tot for loop om zetten toe te voegen aan stapel

--- Menace.py
+++ Menace.py
@@ -10,5 +10,13 @@ class Menace:
         return zetten[kans - 1]
 
 
-    def opbouwen(self, bord, zetten):
+    def opbouwen(self, bord):
+        zetten = bord.mogelijkheden(uniek=True)
         stapel = []
+        for zet in zetten:          #ik wou voor elk "iets" dat in de list zetten zit moet het een zet bij de stapel doen maar dit klopt niet
+            stapel.append(zet)
+
+        return stapel
+
+
+
