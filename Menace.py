2652ffee604ff8b07890fbf65cb50ea6a1775aec
sjourdbeans <sjoerdbuitjes@gmail.com>
2019-01-27 23:37:45 +0100

    ik heb een begin gemaakt aan het opslaan van de zetten. We moeten er voor zorgen dat de zetten worden opgeslagen en niet gereset worden na elke loop.

--- Menace.py
+++ Menace.py
@@ -8,13 +8,22 @@ class Menace:
     def doe_zet(self, bord):
         zetten = bord.mogelijkheden(uniek=True)
         kans = random.randrange(len(zetten))
-        print(self.opbouwen(bord))
-        return zetten[kans - 1]
+        een_zet = zetten[kans-1]
+        gedaan_player1 = []
+        gedaan_player2 = []
+        #print(self.opbouwen(bord))
+        if bord.speler() == 1:              #zetten worden in een list gezet maar na elke beurt gereset doordat de functie opnieuw wordt aangeroepen
+            gedaan_player1.append(een_zet)
+            print('speler 1: ', gedaan_player1)
+        else:
+            gedaan_player2.append(een_zet)
+            print('speler 2: ', gedaan_player2)
+        return een_zet
 
     def opbouwen(self, bord):
         zetten = bord.mogelijkheden(uniek=True)
         stapel = []
-
         for zet in zetten:  #geeft een list met wat de mogelijke zetten in zijn in de toestand van het huidige bord
             stapel.append(zet)
         return stapel
+
